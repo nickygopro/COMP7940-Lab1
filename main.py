fa68082344e7f296827391ad83313e3f8afc9af9
f2457488 <f2457488@csdlabad.local>
2024-01-16 21:06:45 +0800

    With Ex 1, Ex 2 & Ex 3 Done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+def print_factor(x):
+        result = []
+        for i in range(1, x+1):
+                if x%i == 0 and i != 0:
+                        result.append(i)
+        return result
+
 def main():
-	print("Hello World")
+        l = [52633, 8137, 1024, 999]
+        for item in l:
+                print(print_factor(item))
+
 
 if __name__ == '__main__':
 	main()
